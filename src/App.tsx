64c9caa39f263d1bc82b171efb34b458eeb60a5c
israelmanzi <manziisrael99@gmail.com>
2022-12-18 01:36:20 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>
+          <NavBar />
+          <Home />
+        </>} />
+        <Route path="/movie/:id" element={<>
+          <NavBar />
+          <Movie />
+        </>} />
+        <Route path="/search/:search" element={<>
+          <NavBar />
+          <Search />
+        </>} />
+        <Route path="*" element={<>
+          <NavBar />
+          <NotFound />
+        </>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
